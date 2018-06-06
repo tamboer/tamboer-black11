29e850b05ad1efb4c6e9acc7c4e4d865245f63c4
Wil <willem.labuschagne@tvh.com>
2018-06-06 07:14:33 +0200

    first

---
+++ src/main/java/za/co/tamboer/tamboerblack11/TamboerBlack11Application.java
@@ -0,0 +1,12 @@
+package za.co.tamboer.tamboerblack11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamboerBlack11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamboerBlack11Application.class, args);
+	}
+}
---
+++ src/test/java/za/co/tamboer/tamboerblack11/TamboerBlack11ApplicationTests.java
@@ -0,0 +1,16 @@
+package za.co.tamboer.tamboerblack11;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TamboerBlack11ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
