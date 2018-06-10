cec28b30a62dd22774a35d4c81f44dfc935d2caf
Wil <willem.labuschagne@tvh.com>
2018-06-10 11:19:20 +0200

    disable security

--- src/main/java/za/co/tamboer/tamboerblack11/TamboerBlack11Application.java
+++ src/main/java/za/co/tamboer/tamboerblack11/TamboerBlack11Application.java
@@ -2,8 +2,10 @@ package za.co.tamboer.tamboerblack11;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TamboerBlack11Application {
 
 	public static void main(String[] args) {
