67068ebf7d1977da61c7eac9c160512396f1fe31
Wil <willem.labuschagne@tvh.com>
2018-06-11 22:35:53 +0200

    add username and email to resource

--- src/main/java/za/co/tamboer/tamboerblack11/player/web/PlayerResource.java
+++ src/main/java/za/co/tamboer/tamboerblack11/player/web/PlayerResource.java
@@ -8,20 +8,43 @@ import javax.validation.constraints.NotNull;
 public class PlayerResource {
 
     private Long id;
+
     @NotNull
+    private String username;
+    @NotNull
+    private String email;
+
+    @NotNull
+
     private String name;
+
     private String alias;
     @NotNull
     private String gender;
     @NotNull
     private Integer age;
-
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getName() {
