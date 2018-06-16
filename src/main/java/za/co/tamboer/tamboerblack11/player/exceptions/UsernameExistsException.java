d08459f83c4321678cfb4657963b02761fa4c966
Wil Labuschagne <willem.labuschagne@tvh.com>
2018-06-16 23:39:41 +0200

    add correct exception Http status

--- src/main/java/za/co/tamboer/tamboerblack11/player/exceptions/UsernameExistsException.java
+++ src/main/java/za/co/tamboer/tamboerblack11/player/exceptions/UsernameExistsException.java
@@ -1,5 +1,9 @@
 package za.co.tamboer.tamboerblack11.player.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UsernameExistsException extends RuntimeException {
 
     public UsernameExistsException(String exception) {
